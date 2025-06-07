393a80df3b75b28d3995aef49963cac0453c9c50
Patrick Franco <patrickxfranco@gmail.com>
2025-06-07 16:38:25 -0300

    Ajuste no código
    Ajusta o código para remover erros indicados pelo VSCODE de pacotes não utilizados.

--- src/pages/auth/signupForm.tsx
+++ src/pages/auth/signupForm.tsx
@@ -4,7 +4,7 @@ import { z } from 'zod';
 
 import { Button } from '@/components/ui/button';
 import { Checkbox } from '@/components/ui/checkbox';
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Form, FormControl, FormDescription, FormField, FormItem, FormMessage } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { zodResolver } from '@hookform/resolvers/zod';
 
